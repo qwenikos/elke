39fed7ac289a061c44c727ff61c45f61b88c5d08
nikosp <perdikopanis@yahoo.gr>
2017-03-17 16:24:31 +0200

    1

--- Project/js/testApp_module.js
+++ Project/js/testApp_module.js
@@ -4,10 +4,23 @@
 var app=angular.module('testApp', [])
 
 ///////// Create Controller with name submitedCtrl///////////////////
-app.controller('testCtrj', function($scope,$http){
+app.controller('testCtrl', function($scope,$http){
     //debugger;
 	$scope.msg = 'You are now at submited us page';
     console.log("you are in the controler testCtrl ");
+    $scope.sciDirListData = {
+        sciDirList: [
+          {id: '1', name: 'Option A'},
+          {id: '2', name: 'Option B'},
+          {id: '3', name: 'Option C'}
+        ]
+    };
+    
+    $scope.update = function(){
+        //var id = event.target.id;
+        console.log($scope.sciDirSelected);
+        $scope.researchdiv=false;
+    };
 	
 });
 
