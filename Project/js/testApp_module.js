459ef333cd1ce56870cc4085acbb938d7890d7b0
nikosp <perdikopanis@yahoo.gr>
2017-03-23 12:58:56 +0200

    1

--- Project/js/testApp_module.js
+++ Project/js/testApp_module.js
@@ -4,28 +4,75 @@
 var app=angular.module('testApp', [])
 
 ///////// Create Controller with name submitedCtrl///////////////////
-app.controller('testCtrl', function($scope,$http){
+app.controller('testCtrl', function($rootScope,$scope,$http){
     //debugger;
 	$scope.msg = 'You are now at submited us page';
     console.log("you are in the controler testCtrl ");
     $scope.sciDirListData = {
         sciDirList: [
-          {id: '1', name: 'Option A'},
-          {id: '2', name: 'Option B'},
-          {id: '3', name: 'Option C'}
+          {id: '1', name: 'SciDir A'},
+          {id: '2', name: 'SciDir B'},
+          {id: '3', name: 'SciDir C'}
         ]
     };
-    
-    $scope.update = function(){
-        //var id = event.target.id;
-        console.log($scope.sciDirSelected);
-        $scope.researchdiv=false;
+    $scope.researchListData = {
+        researchList: [
+          {id: '1', name: 'Reseach A'},
+          {id: '2', name: 'Reseach B'},
+          {id: '3', name: 'Reseach C'}
+        ]
+    };
+    console.log($scope.researchListData);
+    $scope.updateSciDirSelect = function(){
+        var mylink='services/get_sci_for_fellow.php';
+        var fellowId=1;
+        var myparams='?fellowId='+fellowId;
+        //param='?surname='+surname;
+        var fullurl=mylink+myparams;
+        console.log(fullurl);
+        $http.get(fullurl).then( //success
+		function (response) {
+			$scope.sciDirForFellow = response;
+            console.log("ok");
+            console.log($scope.sciDirForFellow);
+		},
+		function (response) { //fail
+			$scope.error="error in proccessing";
+            console.log("error");
+		}
+        );
+        $scope.researchdiv=true;
+
+        
+    };
+    $scope.updateResearchSelect = function(sciDirSelected){
+        var mylink='services/get_reseach_for_sci_dir_and_fellow.php';
+        var fellowId=1;
+        var sciDirId=sciDirSelected;
+        var myparams='?fellowId='+fellowId+"&sciDirId="+sciDirId;
+        //param='?surname='+surname;
+        var fullurl=mylink+myparams;
+        console.log(fullurl);
+        $http.get(fullurl).then( //success
+		function (response) {
+			$scope.sciDirForFellow = response;
+            console.log("ok");
+            console.log($scope.sciDirForFellow);
+		},
+		function (response) { //fail
+			$scope.error="error in proccessing";
+            console.log("error");
+		}
+        );
+        $scope.researchdiv=true;   
     };
 	
 });
 
-app.run(['$rootScope', '$location', '$http', function ($rootScope, $location, $http) {
+app.run(['$rootScope', '$location', '$http', function ($scope,$rootScope, $location, $http) {
         console.log("you are in the run ");
+        $scope.researchdiv=false;
+        $rootScope.sciDir=-1;
     }
 ]);
 
---
+++ Project/services/get_reseach_for_sci_dir_and_fellow.php
@@ -0,0 +1,42 @@
+<?php
+// get the scientific responsible for fellow_id
+//allow cross site http request
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+//mysqli(mysql_server,username,pass,db)
+$outp = "";
+$goon=false;
+if(isset($_GET['fellow_id'])){
+    $fellow_id=$_GET['fellow_id'];
+    $goon=true;
+}
+else{
+    $fellow_id=-100;
+};
+if(isset($_GET['sciDir_id'])){
+    $sciDir_id=$_GET['sciDir_id'];
+    $goon=true;
+}else{
+   $sciDir_id=-100;
+};
+/*********/
+/*$fellow_id=1;*/
+$goon=true;
+/*******/
+$conn = mysqli_connect('localhost', "root", "qwe123", "elkedb");
+$queryString='SELECT * FROM base_scidir,base_fellow,base_fellow_per_scidir where'.
+' base_fellow_per_scidir.fellow_id=base_fellow.rec_id and'.
+' base_fellow_per_scidir.scidir_id=base_scidir.rec_id and'.
+' base_fellow.rec_id='.$fellow_id;
+//print $queryString;
+$result = mysqli_query($conn,$queryString);
+$data=array();
+//error_log($queryString."\n",3,"/var/tmp/php-mysql.log");
+while($row=mysqli_fetch_array($result)) {
+    $data[]=$row;
+}
+    
+//}
+echo json_encode($data);
+mysqli_close($conn);
+?>
