2e620159583bd5bae4282f6424d59d98f56a0094
nikosp <perdikopanis@yahoo.gr>
2017-03-27 15:03:37 +0300

    1

---
+++ Project/js/autocomplete.js
@@ -0,0 +1,81 @@
+(function () {
+  'use strict';
+  angular
+      .module('autocompleteDemo', ['ngMaterial'])
+      .controller('DemoCtrl', DemoCtrl);
+
+  function DemoCtrl ($timeout, $q, $log) {
+    var self = this;
+
+    self.simulateQuery = false;
+    self.isDisabled    = false;
+
+    // list of `state` value/display objects
+    self.states        = loadAll();
+    self.querySearch   = querySearch;
+    self.selectedItemChange = selectedItemChange;
+    self.searchTextChange   = searchTextChange;
+
+    self.newState = newState;
+
+    function newState(state) {
+      alert("Sorry! You'll need to create a Constitution for " + state + " first!");
+    }
+
+    // ******************************
+    // Internal methods
+    // ******************************
+
+    /**
+     * Search for states... use $timeout to simulate
+     * remote dataservice call.
+     */
+    function querySearch (query) {
+      var results = query ? self.states.filter( createFilterFor(query) ) : self.states,
+          deferred;
+      if (self.simulateQuery) {
+        deferred = $q.defer();
+        $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+        return deferred.promise;
+      } else {
+        return results;
+      }
+    }
+
+    function searchTextChange(text) {
+      $log.info('Text changed to ' + text);
+    }
+
+    function selectedItemChange(item) {
+      $log.info('Item changed to ' + JSON.stringify(item));
+    }
+
+    /**
+     * Build `states` list of key/value pairs
+     */
+    function loadAll() {
+      var allAfm = '059946674, 02234567, 9956789,\
+              03345656,\
+              12345678';
+
+      return allAfm.split(/, +/g).map( function (state) {
+        return {
+          value: state.toLowerCase(),
+          display: state
+        };
+      });
+    }
+
+    /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+
+      return function filterFn(state) {
+        return (state.value.indexOf(lowercaseQuery) === 0);
+      };
+
+    }
+  }
+})();
---
+++ Project/services/insert_doc_into_db.php
@@ -0,0 +1,49 @@
+<?php
+// get the scientific responsible for fellow_id
+//allow cross site http request
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+//mysqli(mysql_server,username,pass,db)
+$outp = "";
+$goon=false;
+if(isset($_GET['fellowId'])){
+    $fellowId=$_GET['fellowId'];
+    $goon=true;
+}
+else{
+    $fellowId=-100;
+};
+
+if(isset($_GET['sciDirId'])){
+    $sciDirId=$_GET['sciDirId'];
+    $goon=true;
+}else{
+   $sciDirId=-100;
+};
+
+
+/*********/
+/*$fellow_id=1;*/
+$goon=true;
+/*******/
+$conn = mysqli_connect('localhost', "root", "qwe123", "elkedb");
+$queryString='SELECT *,base_research.rec_id as base_research_rec_id  FROM base_scidir,base_fellow,base_fellow_per_scidir,'.
+' base_research,base_research_per_scidir where'.
+' base_fellow_per_scidir.fellow_id=base_fellow.rec_id and'.
+' base_fellow_per_scidir.scidir_id=base_scidir.rec_id and'.
+' base_research_per_scidir.scidir_id=base_scidir.rec_id and'.
+' base_research_per_scidir.research_id=base_research.rec_id and'.
+' base_fellow.rec_id='.$fellowId.
+' and base_scidir.rec_id='.$sciDirId;
+//print $queryString;
+$result = mysqli_query($conn,$queryString);
+$data=array();
+//error_log($queryString."\n",3,"/var/tmp/php-mysql.log");
+while($row=mysqli_fetch_array($result)) {
+    $data[]=$row;
+}
+    
+//}
+echo json_encode($data);
+mysqli_close($conn);
+?>
