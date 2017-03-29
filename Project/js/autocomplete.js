83608e833a78b6b511a8d1bb0a4f6f99d87e07aa
nikosp <perdikopanis@yahoo.gr>
2017-03-29 10:02:55 +0300

    ok

--- Project/js/autocomplete.js
+++ Project/js/autocomplete.js
@@ -1,25 +1,21 @@
-(function () {
-  'use strict';
-  angular
-      .module('autocompleteDemo', ['ngMaterial'])
-      .controller('DemoCtrl', DemoCtrl);
+'use strict';
+var app=angular.module('autocompleteApp', ['ngMaterial']);
 
-  function DemoCtrl ($timeout, $q, $log) {
+app.controller('AutoCompleteController', AutoCompleteController);
+  function AutoCompleteController ($scope,$timeout, $q, $log) {
     var self = this;
-
     self.simulateQuery = false;
     self.isDisabled    = false;
-
     // list of `state` value/display objects
     self.states        = loadAll();
+    $scope.states=self.states ;
     self.querySearch   = querySearch;
     self.selectedItemChange = selectedItemChange;
     self.searchTextChange   = searchTextChange;
-
     self.newState = newState;
 
     function newState(state) {
-      alert("Sorry! You'll need to create a Constitution for " + state + " first!");
+      alert("Πρεπει να φτιάξω την function για εισαγωγή νεου ΑΦΜ" + state );
     }
 
     // ******************************
@@ -41,7 +37,29 @@
         return results;
       }
     }
+    
+    /**
+     * Build `states` list of key/value pairs
+     */
+    function loadAll() {
+      var sciDirList= [
+        {value: '059946674', display: 'nikos'},
+        {value: '999999999', display: 'panagiotis'},
+        {value: '344566666', display: 'manolis'}
+      ];
+      return sciDirList;
+    }
 
+    /**
+     * Create filter function for a query string
+     */
+    function createFilterFor(query) {
+      return function filterFn(state) {
+        return (state.value.indexOf(query) === 0);
+      };
+
+    }
+    /*****log the data to console *******/
     function searchTextChange(text) {
       $log.info('Text changed to ' + text);
     }
@@ -49,33 +67,5 @@
     function selectedItemChange(item) {
       $log.info('Item changed to ' + JSON.stringify(item));
     }
-
-    /**
-     * Build `states` list of key/value pairs
-     */
-    function loadAll() {
-      var allAfm = '059946674, 02234567, 9956789,\
-              03345656,\
-              12345678';
-
-      return allAfm.split(/, +/g).map( function (state) {
-        return {
-          value: state.toLowerCase(),
-          display: state
-        };
-      });
-    }
-
-    /**
-     * Create filter function for a query string
-     */
-    function createFilterFor(query) {
-      var lowercaseQuery = angular.lowercase(query);
-
-      return function filterFn(state) {
-        return (state.value.indexOf(lowercaseQuery) === 0);
-      };
-
-    }
   }
-})();
+
