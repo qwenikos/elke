a2bc3ded548a788df95c2b69d0fb2c49e024d6f6
nikosp <perdikopanis@yahoo.gr>
2017-03-29 10:59:21 +0300

    ok

--- Project/js/autocomplete.js
+++ Project/js/autocomplete.js
@@ -7,58 +7,53 @@ app.controller('AutoCompleteController', AutoCompleteController);
     self.simulateQuery = false;
     self.isDisabled    = false;
     // list of `state` value/display objects
-    self.states        = loadAll();
+    self.states = loadAll();
     $scope.states=self.states ;
+    
     self.querySearch   = querySearch;
+    //log
     self.selectedItemChange = selectedItemChange;
     self.searchTextChange   = searchTextChange;
-    self.newState = newState;
+    //create new beneficiary
+    self.insertBenef = insertBenef;
 
-    function newState(state) {
+    function insertBenef(state) {
       alert("Πρεπει να φτιάξω την function για εισαγωγή νεου ΑΦΜ" + state );
     }
 
-    // ******************************
-    // Internal methods
-    // ******************************
-
-    /**
-     * Search for states... use $timeout to simulate
-     * remote dataservice call.
-     */
     function querySearch (query) {
-      var results = query ? self.states.filter( createFilterFor(query) ) : self.states,
-          deferred;
-      if (self.simulateQuery) {
-        deferred = $q.defer();
-        $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
-        return deferred.promise;
-      } else {
-        return results;
+      var results;
+      if (query){
+        results=self.states.filter( createFilterFor(query));
+        
+      }else{
+       results= self.states;
       }
+      return results; 
+    }
+    function createFilterFor(query) {
+      return function filterFn(benef) {
+        return (benef.value.indexOf(query) === 0);
+        
+      };
     }
     
     /**
      * Build `states` list of key/value pairs
      */
+    
     function loadAll() {
-      var sciDirList= [
+      var benefList= [
         {value: '059946674', display: 'nikos'},
         {value: '999999999', display: 'panagiotis'},
         {value: '344566666', display: 'manolis'}
       ];
-      return sciDirList;
+      return benefList;
     }
 
-    /**
-     * Create filter function for a query string
-     */
-    function createFilterFor(query) {
-      return function filterFn(state) {
-        return (state.value.indexOf(query) === 0);
-      };
-
-    }
+    
+    
+    
     /*****log the data to console *******/
     function searchTextChange(text) {
       $log.info('Text changed to ' + text);
