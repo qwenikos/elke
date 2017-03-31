a2086dd43f2fc2397d0571af3510fa008e639b2c
nikosp <perdikopanis@yahoo.gr>
2017-03-31 16:01:46 +0300

    1

--- Project/services/get_doc_entoli_all.php
+++ Project/services/get_doc_entoli_all.php
@@ -12,7 +12,7 @@ $outp = "";
     ' from base_doc_entoli,base_scidir,base_research,base_beneficiary,base_fellow,base_doc_entoli_type where'.
     ' base_doc_entoli.scidir_id=base_scidir.rec_id and'.
     ' base_doc_entoli.research_id=base_research.rec_id and'.
-    ' base_doc_entoli.benef_id=base_beneficiary.rec_id and'.
+    ' base_doc_entoli.benef_afm=base_beneficiary.benef_afm and'.
     ' base_doc_entoli.fellow_id=base_fellow.rec_id and'.
     ' base_doc_entoli.entoli_type_id=base_doc_entoli_type.rec_id';
 //print $queryString;
--- Project/services/insert_doc_into_db.php
+++ Project/services/insert_doc_into_db.php
@@ -4,24 +4,24 @@
     $postdata = file_get_contents("php://input");
     if (isset($postdata)){
         $request = json_decode($postdata);
-        if (property_exists($request,"sciDirSelected")){$sciDirSelected =$request->sciDirSelected ; }else{$sciDirSelected = "";};
-        if (property_exists($request,"researchSelected")){$researchSelected =$request->researchSelected ; }else{$researchSelected = "";};
-        if (property_exists($request,"epistimonikos_name")){$epistimonikos_name =$request->epistimonikos_name ; }else{$epistimonikos_name = "";};
-        if (property_exists($request,"kodikos_ereynas")){$kodikos_ereynams =$request->kodikos_ereynas ; }else{$kodikos_ereynas = "";};
-        if (property_exists($request,"dikaioyxos_afm")){$dikaioyxos_afm =$request->dikaioyxos_afm ; }else{$dikaioyxos_afm = "";};
-        if (property_exists($request,"dikaioyxos_name")){$dikaioyxos_name =$request->dikaioyxos_name ; }else{$dikaioyxos_name = "";};
-        if (property_exists($request,"typos")){if (property_exists($request->typos,"did")){$typos =$request->typos->did ; }} else{$typos= "";}
+        if (property_exists($request,"sciDirSelected")){$sciDirSelected =$request->sciDirSelected ; }else{$sciDirSelected = 'NULL';};
+        if (property_exists($request,"researchSelected")){$researchSelected =$request->researchSelected ; }else{$researchSelected = 'NULL';};
+        if (property_exists($request,"epistimonikos_name")){$epistimonikos_name =$request->epistimonikos_name ; }else{$epistimonikos_name = 'NULL';};
+        if (property_exists($request,"kodikos_ereynas")){$kodikos_ereynams =$request->kodikos_ereynas ; }else{$kodikos_ereynas = 'NULL';};
+        if (property_exists($request,"dikaioyxos_afm")){$dikaioyxos_afm =$request->dikaioyxos_afm ; }else{$dikaioyxos_afm = 'NULL';};
+        if (property_exists($request,"dikaioyxos_name")){$dikaioyxos_name =$request->dikaioyxos_name ; }else{$dikaioyxos_name = 'NULL';};
+        if (property_exists($request,"typos")){if (property_exists($request->typos,"did")){$typos =$request->typos->did ; }} else{$typos= 'NULL';}
         
-        if (property_exists($request,"amoivi_diasthma_apo")){$amoivi_diasthma_apo =$request->amoivi_diasthma_apo ; }else{$amoivi_diasthma_apo = "";};
-        if (property_exists($request,"amoivi_diasthma_eos")){$amoivi_diasthma_eos =$request->amoivi_diasthma_eos ; }else{$amoivi_diasthma_eos = "";};
-        if (property_exists($request,"amoivi_poso")){$amoivi_poso =$request->amoivi_poso ; }else{$amoivi_poso = "";};
-        if (property_exists($request,"prokatavoli_poso")){$prokatavoli_poso =$request->prokatavoli_poso ; }else{$prokatavoli_poso = "";};
-        if (property_exists($request,"prokatavoli_desc")){$prokatavoli_desc =$request->prokatavoli_desc ; }else{$prokatavoli_desc = "";};
+        if (property_exists($request,"amoivi_diasthma_apo")){$amoivi_diasthma_apo =$request->amoivi_diasthma_apo ; }else{$amoivi_diasthma_apo = 'NULL';};
+        if (property_exists($request,"amoivi_diasthma_eos")){$amoivi_diasthma_eos =$request->amoivi_diasthma_eos ; }else{$amoivi_diasthma_eos = 'NULL';};
+        if (property_exists($request,"amoivi_poso")){$amoivi_poso =$request->amoivi_poso ; }else{$amoivi_poso = 'NULL';};
+        if (property_exists($request,"prokatavoli_poso")){$prokatavoli_poso =$request->prokatavoli_poso ; }else{$prokatavoli_poso = 'NULL';};
+        if (property_exists($request,"prokatavoli_desc")){$prokatavoli_desc =$request->prokatavoli_desc ; }else{$prokatavoli_desc = 'NULL';};
         
-        if (property_exists($request,"exoda_ereynwn_desc")){$exoda_ereynwn_desc =$request->exoda_ereynwn_desc ; }else{$exoda_ereynwn_desc = "";};
-        if (property_exists($request,"exoda_ereynwn_poso")){$exoda_ereynwn_poso =$request->exoda_ereynwn_poso ; }else{$exoda_ereynwn_poso = "";};
+        if (property_exists($request,"exoda_ereynwn_desc")){$exoda_ereynwn_desc =$request->exoda_ereynwn_desc ; }else{$exoda_ereynwn_desc = 'NULL';};
+        if (property_exists($request,"exoda_ereynwn_poso")){$exoda_ereynwn_poso =$request->exoda_ereynwn_poso ; }else{$exoda_ereynwn_poso = 'NULL';};
         if (property_exists($request,"check_agora")){$check_agora =$request->check_agora; }else{$check_agora = "0";};
-        if (property_exists($request,"check_diagonismos")){$check_diagonismos =$request->check_diagonismos ; }else{$request->check_agoracheck_diagonismos = "0";};
+        if (property_exists($request,"check_diagonismos")){$check_diagonismos =$request->check_diagonismos ; }else{$check_diagonismos = "0";};
 
         
        //echo  $researchSelect,$epistimonikos_name,$kodikos_ereynams ,$diakioyxos_afm ,$dikaioyxos_name , $typos ,$amoivi_diasthma_apo , $amoivi_diasthma_eos,$amoivi_poso ,$prokatavoli_descprokatavoli_poso,$exoda_ereynwn_desc ,$exoda_ereynwn_poso,$check_agora,$check_diagonismos;
@@ -40,8 +40,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql="INSERT INTO base_doc_entoli (scidir_id,research_id,benef_id,fellow_id,entoli_type_id,paymentof,payment_start_period,peyment_end_period,payment_amount,advance_peyment_desc,advance_payment_amount,research_expenses_desc,reseach_expenses_amount,market_eval,legal_process)
-VALUES ( $sciDirSelected, $researchSelected, $dikaioyxos_afm, '1', $typos, $diakioyxos_afm, $amoivi_diasthma_apo, $amoivi_diasthma_eos, $amoivi_poso, $prokatavoli_poso, $prokatavoli_desc, $exoda_ereynwn_desc, $exoda_ereynwn_poso, $check_agora, $check_diagonismos)";
+$sql="INSERT INTO base_doc_entoli (scidir_id,research_id,benef_afm,fellow_id,entoli_type_id,paymentof,payment_start_period,peyment_end_period,payment_amount,advance_peyment_desc,advance_payment_amount,research_expenses_desc,reseach_expenses_amount,market_eval,legal_process)
+VALUES ( $sciDirSelected, $researchSelected, '$dikaioyxos_afm', '1', $typos, '$dikaioyxos_afm', '$amoivi_diasthma_apo', '$amoivi_diasthma_eos', $amoivi_poso, '$prokatavoli_desc',$prokatavoli_poso,  '$exoda_ereynwn_des', $exoda_ereynwn_poso, $check_agora, $check_diagonismos)";
 echo $sql;
 /*
 $sql = "INSERT INTO base_beneficiary (benef_name, benef_surname, benef_afm, benef_desc)
