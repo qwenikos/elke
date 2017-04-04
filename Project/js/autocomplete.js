ee36b86fece9baf32e131abc5aacce7313c682b2
nikosp <perdikopanis@yahoo.gr>
2017-04-04 15:53:00 +0300

    1

--- Project/js/autocomplete.js
+++ Project/js/autocomplete.js
@@ -4,66 +4,48 @@ var app=angular.module('autocompleteApp', ['ngMaterial', 'ngMessages', 'material
 
 app.controller('AutoCompleteController',
                function AutoCompleteController ($rootScope,$scope,$http,$log,$mdDialog) {
-  $rootScope.loaded=true;
-  $scope.disable_benef_input=true;
-  console.log("incontoler");
-  var self = this;
-  $scope.sub_div_add_beneficiary=false;
-  console.log($scope.sub_div_add_beneficiary)
-  self.isDisabled   = false;
-  var fellowId='1';
-  var action='services/get_beneficiaries.php';
-  var params='?fellowId='+fellowId;
-  var fulURL=action+params
-	$http.get(fulURL).then( //success
-		function (response) {
-      $scope.posts =  response.data;
-      self.states=$scope.posts;
-      self.querySearch   = querySearch;
-      self.selectedItemChange = selectedItemChange;
-      self.searchTextChange   = searchTextChange;
-      //create new beneficiary
-      self.insertBenef = insertBenef;
-		},
-		function (response) { //fail
-			$scope.error="error in proccessing";
-		}
-	);
 
-	
-  function insertBenef($state,$event)  {
-		console.log($event);
-        $mdDialog.show({
-          targetEvent: $event,
-          template:
-            '<md-content>'+
-				'<form name=newBenefForm>'+
-					'<md-input-container>'+
-						'<label>Α.Φ.Μ</label>'+
-						'<input md-maxlength=10 required name=newBenefAfmField ng-model=new_benef_afm>'+
-						'<div ng-messages=newBenefForm.newBenefAfmField.$error><div ng-message=required>Υποχρεωτικό πεδίο</div></div>'+
-						'<!--<div ng-message=md-maxlength>Το ονομα πρέπει να είναι το πολύ 10 χαρακτήρες</div>-->'+
-					'</md-input-container>'+
-					'<md-input-container>'+
-					'<md-input-container>'+
-						'<label>Ονομα</label>'+
-						'<input md-maxlength=30 required name=newBenefnameField ng-model=new_benef_name>'+
-						'<div ng-messages=newBenefForm.newBenefnameField.$error><div ng-message=required>Υποχρεωτικό πεδίο</div></div>'+
-						'<!--<div ng-message=md-maxlength>Το ονομα πρέπει να είναι το πολύ 30 χαρακτήρες</div>-->'+
-					'</md-input-container>'+
-					'<md-input-container>'+
-						'<label>Επίθετο</label>'+
-						'<input md-maxlength=30 required name=newBenefSurnameField ng-model=new_benef_surname>'+
-						'<div ng-messages=newBenefForm.newBenefSurnameField.$error><div ng-message=required>Υποχρεωτικό πεδίο</div></div>'+
-					'</md-input-container>'+
-					'<md-button type="submit">Υποβολή</md-button>'+
-				'</form>'+
-			'</md-content>'
+	$rootScope.loaded=true;
+	$scope.disable_benef_input=true;
+	console.log("in the contoller");
+	var self = this;
+	$scope.sub_div_add_beneficiary=false;
+	self.isDisabled   = false;
+	var fellowId='1';
+	var action='services/get_beneficiaries.php';
+	var params='?fellowId='+fellowId;
+	var fulURL=action+params
+	  $http.get(fulURL).then( //success
+		  function (response) {
+		$scope.posts =  response.data;
+		self.states=$scope.posts;
+		self.querySearch   = querySearch;
+		self.selectedItemChange = selectedItemChange;
+		self.searchTextChange   = searchTextChange;
+		//create new beneficiary
+		self.insertBenef = insertBenef;
+		  },
+		  function (response) { //fail
+			  $scope.error="error in proccessing";
+		  }
+	  );
+	  
+	$scope.save_new_benef=function(){
+		console.log("save new benef to database");	
+	};
+	$scope.close = function() {
+	  $mdDialog.hide();
+	};
+	$scope.insertBenef = function (event)  {
+		$mdDialog.show({
+			scope: $scope,
+			preserveScope: true,
+			controller: 'AutoCompleteController',
+			templateUrl: 'pages/insert_new_benef_dialog.html'
         });
+	};
+  
 
-  
-    }
-  
   
 
   function querySearch (query) {
@@ -85,55 +67,26 @@ app.controller('AutoCompleteController',
 
 
     /*****log the data to console *******/
-  function searchTextChange(text) {
-    $log.info('Text changed to ' + text);
-	console.log(text.length);
-	if (text.length==9) {
-		//alert("fsdafad");
-	};
-  }
-  function selectedItemChange(item) {
-    $log.info('Item changed to ' + JSON.stringify(item));
-    if (item){
-      $scope.autoAFM=item.value;
-      $scope.autoName=item.display;
-	  $scope.disable_benef_input=true;
-    }
-  }
+	function searchTextChange(text) {
+		// $log.info('Text changed to ' + text);
+		 //console.log(text.length);
+		 if (text.length==11) {
+			 //alert("pssss");
+		}
+	
+	}
+	
+	function selectedItemChange(item) {
+		//$log.info('Item changed to ' + JSON.stringify(item));
+		if (item){
+			$scope.autoAFM=item.value;
+			$scope.autoName=item.display;
+			$scope.disable_benef_input=true;
+		}
+	}
   
-  $scope.IsVisible = false;
-	$scope.ShowHide = function () {
-
-	};
-
 });
 
-app.directive('focusOnShow', function($timeout) {
-    return {
-        restrict: 'A',
-        link: function($scope, $element, $attr) {
-            if ($attr.ngShow){
-                $scope.$watch($attr.ngShow, function(newValue){
-                    if(newValue){
-                        $timeout(function(){
-                            $element[0].focus();
-                        }, 0);
-                    }
-                })      
-            }
-            if ($attr.ngHide){
-                $scope.$watch($attr.ngHide, function(newValue){
-                    if(!newValue){
-                        $timeout(function(){
-                            $element[0].focus();
-                        }, 0);
-                    }
-                })      
-            }
-
-        }
-    };
-})
 
 
 
