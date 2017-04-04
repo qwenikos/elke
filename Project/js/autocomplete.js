a6254dc78aa09ea1ea3773f8328fff0627a9a9bb
nikosp <perdikopanis@yahoo.gr>
2017-04-04 13:39:44 +0300

    1

--- Project/js/autocomplete.js
+++ Project/js/autocomplete.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var app=angular.module('autocompleteApp', ['ngMaterial'])
+var app=angular.module('autocompleteApp', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
 
 app.controller('AutoCompleteController',
-               function AutoCompleteController ($rootScope,$scope,$http,$log) {
+               function AutoCompleteController ($rootScope,$scope,$http,$log,$mdDialog) {
   $rootScope.loaded=true;
   $scope.disable_benef_input=true;
   console.log("incontoler");
@@ -29,15 +29,42 @@ app.controller('AutoCompleteController',
 			$scope.error="error in proccessing";
 		}
 	);
-         
 
-  function insertBenef(state) {
-    //alert("Πρεπει να φτιάξω την function για εισαγωγή νεου ΑΦΜ" + state );
-	$scope.disable_benef_input=false;
-	console.log("in insertBenef ");
-	console.log($scope.sub_div_add_beneficiary);
-	var1=false;
-  }
+	
+  function insertBenef($state,$event)  {
+		console.log($event);
+        $mdDialog.show({
+          targetEvent: $event,
+          template:
+            '<md-content>'+
+				'<form name=newBenefForm>'+
+					'<md-input-container>'+
+						'<label>Α.Φ.Μ</label>'+
+						'<input md-maxlength=10 required name=newBenefAfmField ng-model=new_benef_afm>'+
+						'<div ng-messages=newBenefForm.newBenefAfmField.$error><div ng-message=required>Υποχρεωτικό πεδίο</div></div>'+
+						'<!--<div ng-message=md-maxlength>Το ονομα πρέπει να είναι το πολύ 10 χαρακτήρες</div>-->'+
+					'</md-input-container>'+
+					'<md-input-container>'+
+					'<md-input-container>'+
+						'<label>Ονομα</label>'+
+						'<input md-maxlength=30 required name=newBenefnameField ng-model=new_benef_name>'+
+						'<div ng-messages=newBenefForm.newBenefnameField.$error><div ng-message=required>Υποχρεωτικό πεδίο</div></div>'+
+						'<!--<div ng-message=md-maxlength>Το ονομα πρέπει να είναι το πολύ 30 χαρακτήρες</div>-->'+
+					'</md-input-container>'+
+					'<md-input-container>'+
+						'<label>Επίθετο</label>'+
+						'<input md-maxlength=30 required name=newBenefSurnameField ng-model=new_benef_surname>'+
+						'<div ng-messages=newBenefForm.newBenefSurnameField.$error><div ng-message=required>Υποχρεωτικό πεδίο</div></div>'+
+					'</md-input-container>'+
+					'<md-button type="submit">Υποβολή</md-button>'+
+				'</form>'+
+			'</md-content>'
+        });
+
+  
+    }
+  
+  
 
   function querySearch (query) {
     var results;
@@ -55,21 +82,15 @@ app.controller('AutoCompleteController',
       
     };
   }
-    
-  function loadAll() {
-    var benefList= [
-      {value: '059946674', display: 'nikos'},
-      {value: '999999999', display: 'panagiotis'},
-      {value: '344566666', display: 'manolis'}
-    ];
-    console.log("in loadall");
-    console.log('resultslocal', JSON.stringify(benefList));
-    return benefList;
-  }
+
 
     /*****log the data to console *******/
   function searchTextChange(text) {
     $log.info('Text changed to ' + text);
+	console.log(text.length);
+	if (text.length==9) {
+		//alert("fsdafad");
+	};
   }
   function selectedItemChange(item) {
     $log.info('Item changed to ' + JSON.stringify(item));
@@ -80,7 +101,42 @@ app.controller('AutoCompleteController',
     }
   }
   
+  $scope.IsVisible = false;
+	$scope.ShowHide = function () {
+
+	};
+
 });
+
+app.directive('focusOnShow', function($timeout) {
+    return {
+        restrict: 'A',
+        link: function($scope, $element, $attr) {
+            if ($attr.ngShow){
+                $scope.$watch($attr.ngShow, function(newValue){
+                    if(newValue){
+                        $timeout(function(){
+                            $element[0].focus();
+                        }, 0);
+                    }
+                })      
+            }
+            if ($attr.ngHide){
+                $scope.$watch($attr.ngHide, function(newValue){
+                    if(!newValue){
+                        $timeout(function(){
+                            $element[0].focus();
+                        }, 0);
+                    }
+                })      
+            }
+
+        }
+    };
+})
+
+
+
 
 
 
