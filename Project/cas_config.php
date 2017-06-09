7af6fd76cb664a21b0ff957428042b22bace3853
nikosp <perdikopanis@yahoo.gr>
2017-06-09 14:41:44 +0300

    ok

--- Project/cas_config.php
+++ Project/cas_config.php
@@ -1,9 +1,10 @@
 <?php
 
   $cas_protocol  = SAML_VERSION_1_1;
-  $cas_sso_server  = "sso-devel.gunet.gr";
+  //$cas_sso_server  = "sso-devel.gunet.gr";
+  $cas_sso_server  = "sso.uoa.gr";
   $cas_port  = 443;
   $cas_cert  = "/etc/ssl/certs/ca-certificates.crt";
-  $cas_logout_app_redirect_url = "https://identity.gunet.gr/demo/phpCAS/app1";
+  $cas_logout_app_redirect_url = "http://192.168.21.90/nikos/elke/Project/index0_orig1.html";
 
 ?>
